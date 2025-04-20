60df07900f3f055a24728e017d4308a8c89d243e
shlomilev <shlomilev@binata.com>
2025-04-20 11:11:12 +0300

    Initial commit with Dockerfile and render.yaml

---
+++ tcp_server.py
@@ -0,0 +1,16 @@
+import socket
+
+HOST = '0.0.0.0'
+PORT = 5000
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"Listening on {HOST}:{PORT}")
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        data = conn.recv(1024)
+        if data:
+            print(f"Received: {data.decode()}")
+            conn.sendall(b'ACK')
